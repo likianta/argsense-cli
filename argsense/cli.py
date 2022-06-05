f276e96a9c7eab9e4588fea09e48eb34bf8f1d9d
likianta <likianta@foxmail.com>
2022-06-05 23:30:04 +0800

    update: cli (wip)

--- argsense/cli.py
+++ argsense/cli.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import typing as t
+from textwrap import indent
 
 from . import artist
+from .argparse import ParamType
 from .console import console
+from .name_converter import name_2_cname
 from .parser import parse_docstring
 from .parser import parse_function
 
@@ -27,6 +30,7 @@ class T:
     })
     
     _FunctionName = str  # name in snake_case.  # B1
+    _FunctionId = int
     _CommandName = str  # name in kebab-case.  # B2
     CommandsDict = t.Dict[  # A1
         _FunctionName, t.TypedDict('CommandsDict', {
@@ -35,48 +39,158 @@ class T:
             'info': CommandInfo,  # noqa
         })
     ]
+    
+    # WIP proposal
+    CommandsDict2 = t.Dict[
+        _FunctionId, t.TypedDict('FuncInfo', {
+            'func': t.Callable,
+            'cname': str,
+            'args': t.Dict[
+                _ArgName, t.TypedDict('ArgInfo', {
+                    'cname': str,
+                    'ctype': ParamType,  # noqa
+                    'desc': str,
+                })
+            ],
+            'kwargs': t.Dict[
+                _ArgName, t.TypedDict('ArgInfo', {
+                    'cname': str,
+                    'ctype': ParamType,  # noqa
+                    'desc': str,
+                    'default': t.Any,
+                })
+            ],
+        })
+    ]
 
 
 class CommandLineInterface:
     """
     TODO: we will add group feature in future version.
     """
-    commands: T.CommandsDict = {}
-    ''' {
-            str command_name: {
-                'func': callable,
-                'info': dict,
-            }, ...
-        }
-    '''
     
-    def add_command(self, name: str, func, info: T.CommandInfo):
-        self.commands[name] = {
-            'func': func,
-            'info': info,
-        }
+    def __init__(self):
+        self.commands: T.CommandsDict2 = {}
+        self._cname_2_func = {}
+    
+    # -------------------------------------------------------------------------
+    # decorators
+    
+    def cmd(self, name=None) -> t.Callable:
+        """
+        usage:
+            from argsense import cli
+            @cli.cmd()
+            def foo(...):
+                ...
+        """
+        from .parser.func_parser import TParamType as ParamType1
+        from .argparse import ParamType as ParamType2
+
+        def type_2_ctype(t: ParamType1) -> ParamType2:
+            return {
+                'str'  : ParamType2.TEXT,
+                'int'  : ParamType2.NUMBER,
+                'float': ParamType2.NUMBER,
+                'bool' : ParamType2.FLAG,
+                # 'list' : ParamType2.LIST,
+                # 'tuple': ParamType2.LIST,
+                # 'set'  : ParamType2.LIST,
+                # 'dict' : ParamType2.DICT,
+                'any'  : ParamType2.ANY,
+                # 'none': ParamType2.NONE,
+            }.get(t, ParamType2.ANY)
+
+        def decorator(func: t.Callable) -> t.Callable:
+            nonlocal name, type_2_ctype
+            cmd_name = name or func.__name__
+            if cmd_name in self._cname_2_func and \
+                    (new := func) is not (old := self._cname_2_func[cmd_name]):
+                raise Exception(
+                    f'duplicate command name: {cmd_name}',
+                    f'the recorded function is: {old}',
+                    f'the incoming function is: {new}',
+                )
+            
+            func_info = parse_function(func)
+            docs_info = parse_docstring(func.__doc__)
+            
+            self.commands[id(func)] = {
+                'func': func,
+                'cname': name_2_cname(cmd_name),
+                'args': {
+                    name: {
+                        'cname': name_2_cname(name),
+                        'ctype': type_2_ctype(type_),
+                        'desc': docs_info['args'].get(name, ''),
+                    } for name, type_ in func_info['args']
+                },
+                'kwargs': {
+                    name: {
+                        'cname': name_2_cname(name, is_option=True),
+                        'ctype': type_2_ctype(type_),
+                        'desc': docs_info['args'].get(name, ''),
+                        'default': value,
+                    } for name, type_, value in func_info['kwargs']
+                },
+            }
+        
+            return func
+        return decorator
+    
+    # -------------------------------------------------------------------------
+    # run
     
     def run(self, func=None):
+        from .argparse import parse_argv
+        result = parse_argv(
+            argv=sys.argv,
+            mode='group' if not func else 'command',  # noqa
+            front_matter={
+                'args': {} if func is None else {
+                    k: v['ctype']
+                    for k, v in self.commands[id(func)]['args'].items()
+                },
+                # FIXME: if func is None, use global options.
+                'kwargs': {
+                    k: v['ctype']
+                    for k, v in self.commands[id(func)]['kwargs'].items()
+                },
+                'index': {
+                    n: k
+                    for k, v in self.commands[id(func)]['kwargs'].items()
+                    for n in v['cname'].split(';')
+                },
+            }
+        )
+        print(':lv', result)
+        self.show(func)
+        self.exec(func, sys.argv[1:])
+    
+    def show(self, func=None):
         """
         reference:
             [lib:click]/core.py : BaseCommand.main()
         """
         # excerpt
         mode = 'group' if not func else 'command'
+        name = func and func.__name__  # optional[str]
+        desc = '' if mode == 'group' else self.commands[name]['info']['desc']
         args = None if mode == 'group' else tuple(
             x[0].upper()
-            for x in self.commands[func.__name__]['info']['args']
+            for x in self.commands[name]['info']['args']
         )
         has_args = bool(args)
         # has_args = False if mode == 'group' else bool(
-        #     self.commands[func.__name__]['info']['args']
+        #     self.commands[name]['info']['args']
         # )
         
         console.print(
             artist.draw_title(
-                prog_name=_detect_program_name(rich=True),
+                prog_name=_detect_program_name(),
                 commands=bool(mode == 'group'),
                 arguments=args,
+                serif_line=bool(desc),
             ),
             justify='center'
         )
@@ -89,8 +203,17 @@ class CommandLineInterface:
                 })
             )
         
+        if mode == 'command' and desc:
+            # console.print(indent(desc, ' ') + '\n')
+            # console.print(indent(desc, ' '))
+            # TEST
+            from rich.text import Text
+            rich_desc = Text.from_markup(indent(desc, ' '))
+            rich_desc.stylize('grey74')
+            console.print(rich_desc)
+        
         if has_args:
-            keys = (func.__name__,) if func else self.commands.keys()
+            keys = (name,) if func else self.commands.keys()
             for k in keys:
                 v = self.commands[k]['info']
                 console.print(
@@ -107,25 +230,11 @@ class CommandLineInterface:
             justify='right', style='bold', end=' \n'
         )
     
-    # -------------------------------------------------------------------------
-    # decorators
-    
-    def cmd(self, func):
-        func_info = parse_function(func)
-        docs_info = parse_docstring(func.__doc__)
-        self.add_command(
-            func.__name__, func, {
-                'desc'       : docs_info['desc'],
-                'args'       : func_info['args'],
-                'args.docs'  : docs_info['args'],
-                'kwargs'     : func_info['kwargs'],
-                'kwargs.docs': docs_info['opts'],
-            }
-        )
-        return func
+    def exec(self, func, argv):
+        pass
 
 
-def _detect_program_name(rich=False) -> str:
+def _detect_program_name() -> str:
     """
     determine the program name to be `python ...` or `python -m ...`.
     
@@ -142,28 +251,19 @@ def _detect_program_name(rich=False) -> str:
     name = os.path.splitext(os.path.basename(path))[0]
     
     if getattr(main, '__package__', None) is None:
-        if rich:
-            return f'python [bright_red]{name}.py[/]'
-        else:
-            return f'python {name}.py'
+        return f'python {name}.py'
     elif (
             os.name == 'nt'
             and main.__package__ == ''
             and not os.path.exists(path)
             and os.path.exists(f'{path}.exe')
     ):
-        if rich:
-            return f'[bright_red]{name}.exe[/]'
-        else:
-            return f'{name}.exe'
+        return f'{name}.exe'
     
     py_module = t.cast(str, main.__package__)
     if name != '__main__':  # a submodule like 'example.cli'
         py_module = f'{py_module}.{name}'.lstrip('.')
-    if rich:
-        return f'python -m [bright_red]{py_module}[/]'
-    else:
-        return f'python -m {py_module}'
+    return f'python -m {py_module}'
 
 
 cli = CommandLineInterface()
