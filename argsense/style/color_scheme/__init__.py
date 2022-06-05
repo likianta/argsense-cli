036d6c5f105cf61d28920369a7834c72e57beabb
likianta <likianta@foxmail.com>
2022-06-05 23:23:05 +0800

    feat: create style package (wip)

---
+++ argsense/style/color_scheme/__init__.py
@@ -0,0 +1,2 @@
+from .default import Color as DefaultColor
+from .default import Gradient as DefaultGradient
---
+++ argsense/style/color_scheme/default.py
@@ -0,0 +1,16 @@
+"""
+inspired by [oh-my-posh/themes/bubbles <https://github.com/JanDeDobbeleer/oh-my-
+posh/blob/main/themes/bubbles.omp.json>]
+"""
+
+
+class Color:
+    deep_gray = 'grey74'  # for command description.
+    dim_acrylic = '#29315a'  # for text background.
+    magenta = 'magenta'  # accent color for command related stuff.
+    scarlet = '#e64747'  # prog name, error panel, etc.
+
+
+class Gradient:
+    blue = ('#0a87ee', '#9294f0')  # calm blue -> light blue
+    red = ('#ed3b3b', '#d08bf3')  # rose red -> violet
