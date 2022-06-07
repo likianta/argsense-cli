3a1953c64ecaf9bbf85a489013bdfb7e26ea3aa4
likianta <likianta@foxmail.com>
2022-06-07 11:16:35 +0800

    style: adjust param type and typing

--- argsense/converter.py
+++ argsense/converter.py
@@ -1,15 +1,18 @@
 import typing as t
-from .argparse import ParamType as ParamType2
-from .parser.func_parser import TParamType as ParamType1
+
+from .argparse import ParamType
 
 __all__ = ['name_2_cname', 'type_2_ctype']
 
 
 class T:
-    PresetStyle = t.Literal['grp', 'cmd', 'arg', 'opt', 'ext']
+    from .parser.func_parser import TParamType as _ParamType
+    ParamType1 = _ParamType
+    ParamType2 = ParamType
+    Style = t.Literal['grp', 'cmd', 'arg', 'opt', 'ext']
 
 
-def name_2_cname(name: str, style: T.PresetStyle = None) -> str:
+def name_2_cname(name: str, style: T.Style = None) -> str:
     # convert name from snake_case to kebab-case.
     name = name.lstrip('_').replace('_', '-')
     if style == 'arg':
@@ -19,16 +22,16 @@ def name_2_cname(name: str, style: T.PresetStyle = None) -> str:
     return name
 
 
-def type_2_ctype(t: ParamType1) -> ParamType2:
+def type_2_ctype(t: T.ParamType1) -> T.ParamType2:
     return {
-        'str'  : ParamType2.TEXT,
-        'int'  : ParamType2.NUMBER,
-        'float': ParamType2.NUMBER,
-        'bool' : ParamType2.FLAG,
-        # 'list' : ParamType2.LIST,
-        # 'tuple': ParamType2.LIST,
-        # 'set'  : ParamType2.LIST,
-        # 'dict' : ParamType2.DICT,
-        'any'  : ParamType2.ANY,
-        # 'none' : ParamType2.NONE,
-    }.get(t, ParamType2.ANY)
+        'str'  : ParamType.TEXT,
+        'int'  : ParamType.NUMBER,
+        'float': ParamType.NUMBER,
+        'bool' : ParamType.FLAG,
+        # 'list' : ParamType.LIST,
+        # 'tuple': ParamType.LIST,
+        # 'set'  : ParamType.LIST,
+        # 'dict' : ParamType.DICT,
+        'any'  : ParamType.ANY,
+        # 'none' : ParamType.NONE,
+    }.get(t, ParamType.ANY)
