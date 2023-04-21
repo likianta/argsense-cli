dba61e8333df575cf8806f85f9801953bbc26177
likianta <likianta@foxmail.com>
2023-04-21 17:29:33 +0800

    refactor: decouple renderer from cli

--- argsense/cli.py
+++ argsense/cli.py
@@ -4,13 +4,12 @@ import os
 import sys
 import typing as t
 
-from . import artist
 from . import config
+from . import render
 from .argparse import ParamType
 from .console import console
 from .parser import parse_docstring
 from .parser import parse_function
-from .style import palette
 
 __all__ = ['CommandLineInterface', 'cli']
 
@@ -90,6 +89,7 @@ class CommandLineInterface:
         func_info = parse_function(func, fallback_type=config.FALLBACK_TYPE)
         docs_info = parse_docstring(func.__doc__ or '')
         
+        # noinspection PyTypeChecker
         self.commands[id(func)] = {
             'func'          : func,
             'cname'         : cmd_name,
@@ -205,18 +205,18 @@ class CommandLineInterface:
             #   currently `--help` and `--helpx` are the only two global
             #   options.
             if result['kwargs'].get(':helpx'):
-                self.show2()
+                render.render_cli_2(self)
             else:
-                self.show(func, show_func_name_in_title=bool(mode == 'group'))
+                render.render_cli(self, func, show_func_name_in_title=bool(mode == 'group'))
         else:
             if result['kwargs'].get(':help') or \
                     result['kwargs'].get(':helpx'):
                 if '**' in func_info['kwargs']:
                     if not func_info['transport_help']:
-                        self.show(func, show_func_name_in_title=True)
+                        render.render_cli(self, func, show_func_name_in_title=True)
                         return
                 else:
-                    self.show(func, show_func_name_in_title=True)
+                    render.render_cli(self, func, show_func_name_in_title=True)
                     return
             
             try:
@@ -227,327 +227,9 @@ class CommandLineInterface:
                         result['kwargs'].get(':help') or
                         result['kwargs'].get(':helpx')
                 ):
-                    self.show(func, show_func_name_in_title=True)
+                    render.render_cli(self, func, show_func_name_in_title=True)
                 else:
                     console.print_exception()
-    
-    def show(self, func, **kwargs):
-        """
-        reference: [lib:click/core.py : BaseCommand.main()]
-        
-        kwargs:
-            show_func_name_in_title: bool[default True]
-        """
-        is_group: bool
-        has_args: bool
-        has_kwargs: bool
-        
-        if func is None:
-            is_group = True
-            has_args = False
-            has_kwargs = False
-            
-            console.print(
-                artist.draw_title(
-                    prog_name=_detect_program_name(),
-                    func_name='FUNCTION',
-                    args=(),
-                    kwargs=('...',)
-                ), justify='center'
-            )
-            
-            console.print(
-                artist.draw_commands_panel((
-                    (v['cname'], v['desc'])  # noqa
-                    for v in self.commands.values()
-                ))
-            )
-        
-        else:
-            func_info = self.commands[id(func)]
-            desc = func_info['desc']
-            is_group = False
-            has_args = bool(func_info['args'])
-            has_kwargs = bool(func_info['kwargs'])
-            
-            # experimental
-            if config.ALIGN_ARGS_AND_OPTS_FIELD_WIDTH:
-                if has_args and has_kwargs:
-                    config.Dynamic.PREFERRED_FIELD_WIDTH_OF_NAME = max((
-                        *map(len, (x['cname']
-                                   for x in func_info['args'].values())),
-                        *map(len, (x['cname'].replace(',', ', ')
-                                   for x in func_info['kwargs'].values())),
-                    ))
-                    # print(Dynamic.PREFERRED_FIELD_WIDTH_OF_NAME, ':v')
-            
-            from textwrap import indent
-            if (has_args or has_kwargs) or (not desc):
-                console.print(
-                    artist.draw_title(
-                        prog_name=_detect_program_name(),
-                        func_name=func_info['cname']
-                        if kwargs.get('show_func_name_in_title', True)
-                        else '',
-                        args=tuple(
-                            v['cname'] for v in func_info['args'].values()
-                        ),
-                        kwargs=tuple(
-                            v['cname'].split(',', 1)[0]
-                            for v in func_info['kwargs'].values()
-                        )
-                    ), justify='center'
-                )
-                if desc: console.print(indent(desc, ' '))
-            else:
-                # assert desc
-                console.print('\n'.join((
-                    '',
-                    indent(artist.draw_title(
-                        prog_name=_detect_program_name(),
-                        func_name=func_info['cname']
-                        if kwargs.get('show_func_name_in_title', True)
-                        else '',
-                        args=tuple(
-                            v['cname'] for v in func_info['args'].values()
-                        ),
-                        kwargs=tuple(
-                            v['cname'].split(',', 1)[0]
-                            for v in func_info['kwargs'].values()
-                        ),
-                        add_serif_line=True,
-                    ), '    '),
-                    indent(f'[grey74]{desc}[/]', '    '),
-                    ''
-                )))
-            
-            if args := func_info['args']:
-                console.print(
-                    artist.draw_arguments_panel((
-                        (v['cname'], v['ctype'].name, v['desc'])
-                        for v in args.values()
-                    ))
-                )
-            
-            if kwargs := func_info['kwargs']:
-                console.print(
-                    artist.draw_options_panel((
-                        (v['cname'].replace(',', ', '),
-                         v['ctype'].name,
-                         v['desc'],
-                         '[red dim](default={})[/]'.format(
-                             v['default'] if v['default'] != '' else '""'
-                         ))
-                        for v in kwargs.values()
-                    ))
-                )
-        
-        # show logo in right-bottom corner.
-        if not is_group and not has_args and not has_kwargs:
-            return
-        # noinspection PyTypeChecker
-        console.print(
-            artist.post_logo(
-                style='group' if is_group
-                else 'option' if has_kwargs
-                else 'argument'
-            ),
-            justify='right', style='bold', end=' \n'
-        )
-    
-    def show2(self):
-        """
-        warning: this is an experimental method for testing new style of 'helpx'.
-        """
-        from rich.align import Align
-        from rich.padding import Padding
-        
-        def show(func: t.Optional[T.Func], show_logo: bool) -> dict:
-            """
-            warning:
-                this function is not the same with [self.show()]. some minor
-                changes are made.
-                if we want to merge them into one in the future, be careful
-                comparing the difference between them.
-            """
-            is_group: bool
-            has_args: bool
-            has_kwargs: bool
-            
-            collect_renderables = {
-                'title'    : None,
-                'desc'     : None,
-                'cmd_panel': None,
-                'arg_panel': None,
-                'opt_panel': None,
-                'logo'     : None,
-            }
-            
-            if func is None:
-                is_group = True
-                has_args = False
-                has_kwargs = False
-                
-                collect_renderables['title'] = Align.center(
-                    artist.draw_title(
-                        prog_name=_detect_program_name(),
-                        func_name='FUNCTION',
-                        args=(),
-                        kwargs=('...',)
-                    )
-                )
-                
-                collect_renderables['cmd_panel'] = (
-                    artist.draw_commands_panel((
-                        (v['cname'], v['desc'])  # noqa
-                        for v in self.commands.values()
-                    ))
-                )
-            
-            else:
-                func_info = self.commands[id(func)]
-                desc = func_info['desc']
-                is_group = False
-                has_args = bool(func_info['args'])
-                has_kwargs = bool(func_info['kwargs'])
-                
-                # experimental
-                if config.ALIGN_ARGS_AND_OPTS_FIELD_WIDTH:
-                    if has_args and has_kwargs:
-                        config.Dynamic.PREFERRED_FIELD_WIDTH_OF_NAME = max((
-                            *map(len, (x['cname']
-                                       for x in func_info['args'].values())),
-                            *map(len, (x['cname'].replace(',', ', ')
-                                       for x in func_info['kwargs'].values())),
-                        ))
-                        # print(Dynamic.PREFERRED_FIELD_WIDTH_OF_NAME, ':v')
-                
-                collect_renderables['title'] = Align.center(
-                    artist.draw_title(
-                        prog_name=_detect_program_name(),
-                        func_name=func_info['cname'],
-                        args=tuple(
-                            v['cname'] for v in func_info['args'].values()
-                        ),
-                        kwargs=tuple(
-                            v['cname'].split(',', 1)[0]
-                            for v in func_info['kwargs'].values()
-                        ),
-                    )
-                )
-                if desc:
-                    from textwrap import indent
-                    collect_renderables['desc'] = indent(desc, ' ')
-                elif not (has_args or has_kwargs):  # no panel besides the desc.
-                    collect_renderables['desc'] = ' ' + config.FALLBACK_DESC
-                
-                if args := func_info['args']:
-                    collect_renderables['arg_panel'] = (
-                        artist.draw_arguments_panel((
-                            (v['cname'], v['ctype'].name, v['desc'])
-                            for v in args.values()
-                        ))
-                    )
-                
-                if kwargs := func_info['kwargs']:
-                    collect_renderables['opt_panel'] = (
-                        artist.draw_options_panel((
-                            (v['cname'].replace(',', ', '),
-                             v['ctype'].name,
-                             v['desc'],
-                             '[red dim](default={})[/]'.format(v['default']))
-                            for v in kwargs.values()
-                        ))
-                    )
-            
-            # show logo in right-bottom corner.
-            if show_logo:
-                if not is_group and not has_args and not has_kwargs:
-                    return collect_renderables
-                # noinspection PyTypeChecker
-                collect_renderables['logo'] = Padding(
-                    Align.right(
-                        artist.post_logo(
-                            style='command'
-                        ), style='bold'
-                    ), (0, 1, 0, 0)
-                )
-            
-            return collect_renderables
-        
-        parts = []  # noqa
-        parts.append(show(None, show_logo=True))
-        parts.extend(show(v['func'], show_logo=False)  # noqa
-                     for v in self.commands.values())
-        
-        def render():
-            # from rich.columns import Columns
-            from rich.console import Group
-            from rich.panel import Panel
-            
-            # preferred_field_width = {
-            #     'command_field': max(
-            #         map(len, (v['cname']
-            #                   for v in self.commands.values()))
-            #     ),
-            #     'param_field'  : max((
-            #         *map(len, (w['cname']
-            #                    for v in self.commands.values()
-            #                    for w in v['args'].values())),
-            #         *map(len, (w['cname'].replace(',', ', ')
-            #                    for v in self.commands.values()
-            #                    for w in v['kwargs'].values())),
-            #     )),
-            #     'type_field'   : len('NUMBER'),
-            # }
-            # print(preferred_field_width, ':v')
-            
-            def tint(text: str, color: str) -> str:
-                # a simple function to tint a text snippet.
-                return f'[{color}]{text}[/]'
-            
-            console.print(parts[0]['title'])
-            assert not parts[0]['desc']
-            
-            group = []
-            for index, func_info in enumerate(self.commands.values()):
-                cmd_name = func_info['cname']  # noqa
-                group.append(tint(
-                    f' {cmd_name} ',
-                    f'b {palette.panel.command_highlight}'
-                ))
-                
-                sub_part = parts[index + 1]
-                sub_panel = Padding(Panel(
-                    Group(*filter(
-                        lambda x: x is not None,
-                        (
-                            # sub_part['title'],
-                            sub_part['desc'],
-                            sub_part['arg_panel'],
-                            sub_part['opt_panel'],
-                        )
-                    )),
-                    border_style=palette.panel.border.command,
-                    title=str(sub_part['title']).split('\\n')[1].replace(
-                        '\\[OPTIONS]', '[OPTIONS]'
-                    ),
-                    title_align='center',
-                ), pad=(0, 0, 0, 4))
-                group.append(sub_panel)
-                
-                group.append('')
-            
-            assert group[-1] == ''
-            group[-1] = parts[0]['logo']
-            
-            # TODO: (below) both A and B are good, i would make it customizable
-            #   in the future.
-            return Panel(Group(*group),
-                         border_style=palette.panel.border.group)  # A
-            # return Group(*group)  # B
-        
-        console.print(render())
 
 
 def _detect_program_name() -> str:
---
+++ argsense/render/__init__.py
@@ -0,0 +1,2 @@
+from .rich import render as render_cli
+from .rich import render2 as render_cli_2
